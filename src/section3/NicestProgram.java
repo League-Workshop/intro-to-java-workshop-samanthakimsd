368a742e7a0251213b0869336d23825ced39791f
samanthakimsd <league@WTS14-4.attlocal.net>
2018-06-20 11:58:10 -0700

    i completed candy crush, greeter, hello world, madlibs, nicest program, speak and spell, stephenhawkings, and world domination

---
+++ src/section3/NicestProgram.java
@@ -0,0 +1,14 @@
+package section3;
+
+import javax.swing.JOptionPane;
+
+public class NicestProgram {
+	public static void main(String[] args) {
+
+		JOptionPane.showMessageDialog(null, "You are smart and good at coding");
+		JOptionPane.showMessageDialog(null, "You have a cute style");
+		JOptionPane.showMessageDialog(null, "You are pretty");
+		System.out.println("Waffles FSHFOHAIHFIJAFIHIF");
+		JOptionPane.showInputDialog("What is your name?");
+	}
+}
